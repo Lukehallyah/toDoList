878d3e033563f4fafbab646be329d610dc272800
Luke <slukehall92@gmail.com>
2026-05-17 16:46:22 -0400

    Rework of Popup for toDos data entry.

--- src/entryPopUp.js
+++ src/entryPopUp.js
@@ -21,11 +21,93 @@ let addPopUp = ()=>{
             buttonHolder.appendChild(removeButton);
         }
 
+    removeButton.addEventListener('click', ()=>{
+
+        main.removeChild(popUp);
+    })
+
+
+
+
     const title = document.createElement('p')
         if(title){
             title.classList.add('title');
             title.textContent='Add a New Task!';
+            title.style.fontWeight='600';
             popUp.appendChild(title);
+        }
+
+    const toDoForm = document.createElement('form');
+        if(toDoForm){
+            toDoForm.style.width='100%';
+            toDoForm.style.height='75%';
+            toDoForm.style.border='2px solid black';
+            toDoForm.style.display='flex';
+            toDoForm.style.flexDirection='column';
+            popUp.appendChild(toDoForm);
+        }
+
+    const titleId="taskTitle";
+    const labelTitle= document.createElement('label');
+        if(labelTitle){
+            labelTitle.style.width='25%';
+            labelTitle.style.height='100%';
+            labelTitle.textContent="Title : "
+            labelTitle.style.fontSize='1.5rem';
+            labelTitle.style.display='flex';
+            labelTitle.style.alignItems='center';
+            labelTitle.style.justifyContent='center';
+            labelTitle.htmlFor=titleId;
+        }
+    const titleInput = document.createElement('input');
+        if(titleInput){
+            titleInput.style.width='70%';
+            titleInput.style.height='100%';
+            titleInput.id=titleId;
+            titleInput.placeholder='Enter task title here...'
+
+        }        
+    const titleBoxHolder= document.createElement('div');
+        if(titleBoxHolder){
+            titleBoxHolder.style.width='100%';
+            titleBoxHolder.style.height='20%';
+            // titleBoxHolder.style.border='2px solid blue';
+            titleBoxHolder.style.display='flex';
+            titleBoxHolder.appendChild(labelTitle);
+            titleBoxHolder.appendChild(titleInput);
+            toDoForm.appendChild(titleBoxHolder);
+        }
+
+
+    const taskId = "taskId";
+    const labelTaskDesc = document.createElement('label');
+        if(labelTaskDesc){
+            labelTaskDesc.htmlFor=taskId;
+            labelTaskDesc.textContent='Task Description';
+            labelTaskDesc.style.fontSize='1.25rem';
+            labelTaskDesc.style.textAlign='left';
+        }
+    const inputTaskDesc = document.createElement('textarea');
+        if(inputTaskDesc){
+            inputTaskDesc.id=taskId;
+            inputTaskDesc.style.height='60%';
+            inputTaskDesc.style.width='80%';
+            inputTaskDesc.style.paddingLeft='10%';
+        }
+
+
+
+
+    const taskDescBox = document.createElement('div');
+        if(taskDescBox){
+            taskDescBox.style.height='80%';
+            taskDescBox.style.width='100%';
+            taskDescBox.style.border='2px solid green';
+            taskDescBox.style.display='flex';
+            taskDescBox.style.flexDirection='column';
+            taskDescBox.appendChild(labelTaskDesc);
+            taskDescBox.appendChild(inputTaskDesc);
+            toDoForm.appendChild(taskDescBox);
         }
 
 }
